0f3472fb868c2b5606540016a157c81c4eba9a33
ShaunMYoung <smyoung@ufl.edu>
2019-03-02 20:10:36 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form action="list.js">
+          <input type="text" name="reminder" id="reminder" placeholder="Type a reminder here..." required=""></input>
+          <input type="submit" name="add" value="Add" id="submit"></input>
+        </form>
+        <ul class="list">
+          <li>Buy groceries</li>
+          <li>Finish group project</li>
+        </ul>
       </div>
     );
   }
